52db1fc65d708afa09a9265506bacd8cc7f754c1
Debayan Kar <kardebayan3@gmail.com>
2025-02-19 21:49:37 +0530

    salaa: Drop `libmtkperf_client` from tree

--- device.mk
+++ device.mk
@@ -305,6 +305,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
     hardware/oplus
 
 # Thermal
